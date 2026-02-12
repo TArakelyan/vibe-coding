a4c36dfe0d3cda3effd6f1cce679d268b5e9d7a8
TArakelyan <150421810+TArakelyan@users.noreply.github.com>
2026-02-12 10:58:56 +0300

    Delete projects/betting-calculator-sports directory

--- projects/betting-calculator-sports/resize.js
+++
@@ -1,7 +0,0 @@
-// ?debug=true
-const queryParams = new URLSearchParams(window.location.search);
-const debug = queryParams.get('debug');
-
-if (debug && window.location.hostname === "specials.cdn.sports.ru") {
-    window.location.href = 'https://sirena.sports.ru/hockeypicker/';
-}
